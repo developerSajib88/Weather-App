57ac0f085e9d140b4013f4a8eb27902993a1924d
developerSajib88 <developersajib88@gmail.com>
2024-12-24 22:02:55 +0600

    dependency inject between dashboard domain and infrastructure.

--- lib/core/dependency_injection/dependency_injection.dart
+++ lib/core/dependency_injection/dependency_injection.dart
@@ -1,11 +1,16 @@
+import 'package:feature_first/features/dashboard/application/dashboard_state.dart';
+import 'package:feature_first/features/dashboard/application/dashboard_state_notifier.dart';
+import 'package:feature_first/features/dashboard/domain/dashboard_domain.dart';
+import 'package:feature_first/features/dashboard/infrastructure/dashboard_dom_i.dart';
 import 'package:get_it/get_it.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 part 'dependency_injection_provider.dart';
 
 final getIt = GetIt.instance;
 
 void init() {
-  // Example
-  //getIt.registerLazySingleton<AuthenticationDom>(()=> AuthenticationDomI());
+
+  getIt.registerLazySingleton<DashboardDomain>(()=> DashboardDomainI());
 
 }
