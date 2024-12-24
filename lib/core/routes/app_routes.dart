539c026a80b3f2882767f0e5d69b20dc80c52faa
developerSajib88 <developersajib88@gmail.com>
2024-12-24 21:44:38 +0600

    Dashboard architecture create.

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,4 +1,5 @@
 import 'package:feature_first/core/routes/routes_error_page.dart';
+import 'package:feature_first/features/dashboard/presentation/dashboard_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
 
@@ -6,7 +7,7 @@ class AppRoutes{
 
   static GoRouter routes = GoRouter(
       debugLogDiagnostics: true,
-      //initialLocation: SplashScreen.path,
+      initialLocation: DashboardScreen.path,
       errorBuilder: (BuildContext context, GoRouterState state) =>
           RouteErrorPage(state: state),
       redirect: (BuildContext context, GoRouterState state){
@@ -14,33 +15,19 @@ class AppRoutes{
       },
       routes: [
 
-        //Splash Screen Route And Initial route
-        // GoRoute(
-        //   path: SplashScreen.path,
-        //   pageBuilder: (context, state) {
-        //     return CustomTransitionPage<void>(
-        //         key: state.pageKey,
-        //         child: const SplashScreen(),
-        //         transitionsBuilder: (context, animation, secondaryAnimation, child) =>
-        //             CupertinoPageTransition(primaryRouteAnimation: animation, secondaryRouteAnimation: secondaryAnimation, linearTransition: true, child: child)
-        //     );
-        //   },
-        // ),
+        // Dashboard Screen Route And Initial route
+        GoRoute(
+          path: DashboardScreen.path,
+          pageBuilder: (context, state) {
+            return CustomTransitionPage<void>(
+                key: state.pageKey,
+                child: const DashboardScreen(),
+                transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+                    CupertinoPageTransition(primaryRouteAnimation: animation, secondaryRouteAnimation: secondaryAnimation, linearTransition: true, child: child)
+            );
+          },
+        ),
 
-
-        // //Home Screen Route
-        // GoRoute(
-        //     path: HomeScreen.path,
-        //     name: HomeScreen.name,
-        //     pageBuilder: (context, state) {
-        //       return CustomTransitionPage<void>(
-        //           key: state.pageKey,
-        //           child: const HomeScreen(),
-        //           transitionsBuilder: (context, animation, secondaryAnimation, child) =>
-        //               CupertinoPageTransition(primaryRouteAnimation: animation, secondaryRouteAnimation: secondaryAnimation, linearTransition: true, child: child)
-        //       );
-        //     },
-        // ),
 
 
       ]
---
+++ lib/features/dashboard/infrastructure/dashboard_dom_i.dart
@@ -0,0 +1,3 @@
+import 'package:feature_first/features/dashboard/domain/dashboard_domain.dart';
+
+class DashboardDomainI implements DashboardDomain{}
