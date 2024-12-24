2896973532b5df2dce3e2a752ffcae092a862e2c
developerSajib88 <developersajib88@gmail.com>
2024-12-24 22:43:16 +0600

    Weather api urls and api services create for fetching weather data.

--- lib/data/api/api_urls/api_urls.dart
+++ lib/data/api/api_urls/api_urls.dart
@@ -1,12 +1,16 @@
 class ApiUrls{
 
+  ///API Key
+  static String apiKey = "1365a6be959c4efd8ae161513242412&";
+
+
   /// Development Base Url
-  static String get baseUrl => "http://159.223.38.189:3333";
+  /// static String get baseUrl => "http://159.223.38.189:3333";
 
   /// Production Base Url
-  //static String get baseUrl => "https://baseurl.com";
+  static String get baseUrl => "  https://api.weatherapi.com";
 
   ///Api Urls
-  static String get createAccount => "$baseUrl/create-account";
+  static String get forecastUri => "$baseUrl/v1/forecast.json?key=$apiKey";
 
 }
