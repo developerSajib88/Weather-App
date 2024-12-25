7bb67286f326c7992df3594ff7a6d32e838f6be8
developerSajib88 <developersajib88@gmail.com>
2024-12-25 14:17:06 +0600

    Custom Text Style modification.

--- lib/utils/styles/custom_text_styles.dart
+++ lib/utils/styles/custom_text_styles.dart
@@ -13,13 +13,13 @@ class CustomTextStyles {
 
   static TextStyle secondary = GoogleFonts.poppins(
     color: ColorPalates.defaultWhite,
-    fontSize: 8.sp,
+    fontSize: 7.sp,
     fontWeight: FontWeight.w600,
     height: 1.07,
   );
 
   static TextStyle primaryBold = GoogleFonts.poppins(
-    fontSize: 8.sp,
+    fontSize: 7.sp,
     color: ColorPalates.defaultWhite,
     fontWeight: FontWeight.w600,
   );
