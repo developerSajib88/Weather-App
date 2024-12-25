fb158d6838060b3923831658bd34265425ecf0ac
developerSajib88 <developersajib88@gmail.com>
2024-12-25 14:19:16 +0600

    Weather of to days widget create.

--- lib/features/dashboard/presentation/dashboard_screen.dart
+++ lib/features/dashboard/presentation/dashboard_screen.dart
@@ -1,5 +1,10 @@
+import 'package:feature_first/common/widgets/components/background_container.dart';
+import 'package:feature_first/features/dashboard/presentation/weather_info_card.dart';
+import 'package:feature_first/features/dashboard/presentation/weather_of_nextdays.dart';
+import 'package:feature_first/features/dashboard/presentation/weather_of_today.dart';
+import 'package:feature_first/generated/assets.dart';
+import 'package:feature_first/utils/utils.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class DashboardScreen extends HookConsumerWidget {
@@ -12,9 +17,129 @@ class DashboardScreen extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: Container(
-        width: 1.sw,
-        height: 1.sh,
+
+      backgroundColor: ColorPalates.secondaryColor,
+      body: BackgroundContainer(
+        padding: EdgeInsets.fromLTRB(6.w, 6.h, 6.w, 0),
+        child: SafeArea(
+          child: Column(
+            children: [
+          
+              Row(
+                mainAxisAlignment: mainSpaceBetween,
+                children:[
+          
+                  Opacity(
+                    opacity: 0.30,
+                    child: Container(
+                      width: 20.w,
+                      height: 20.w,
+                      decoration: BoxDecoration(
+                        color: ColorPalates.defaultWhite,
+                        borderRadius: radius4
+                      ),
+                      child: Icon(
+                        Icons.search,
+                      ),
+                    ),
+                  ),
+
+
+                  Text(
+                      "London",
+                    style: CustomTextStyles.primary,
+                  ),
+
+
+                  Opacity(
+                    opacity: 0.30,
+                    child: Container(
+                      width: 20.w,
+                      height: 20.w,
+                      decoration: BoxDecoration(
+                          color: ColorPalates.defaultWhite,
+                          borderRadius: radius4
+                      ),
+                      child: Icon(
+                        Icons.gps_fixed,
+                      ),
+                    ),
+                  ),
+
+          
+                ]
+              ),
+
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      gap12,
+                      
+                      Text(
+                        "Mostly Sunny",
+                        style: CustomTextStyles.secondary,
+                      ),
+                      
+                      gap6,
+                      
+                      Image.asset(
+                        Assets.imagesCloud,
+                        width: 100.w,
+                        height: 70.w,
+                      ),
+                      
+                      
+                      Row(
+                        mainAxisAlignment: mainCenter,
+                        crossAxisAlignment: crossStart,
+                        children: [
+                          Text(
+                            "23",
+                            style: GoogleFonts.poppins(
+                              color: ColorPalates.defaultWhite,
+                              fontSize: 30.sp,
+                              fontWeight: FontWeight.w600
+                            ),
+                          ),
+                      
+                          Text(
+                            "o",
+                            style: CustomTextStyles.primaryBold,
+                          )
+                          
+                        ],
+                      ),
+                      
+                      Text(
+                        "Friday, 26 August 2022 | 10:00",
+                        style: CustomTextStyles.primary,
+                      ),
+                      
+                      gap12,
+                      
+                      const WeatherInfoCard(),
+                      
+                      gap8,
+                      
+                      const WeatherOfToday(),
+                      
+                      gap8,
+                      
+                      const WeatherOfNextDays(),
+
+                      gap48
+
+                    ],
+                  ),
+                ),
+              ),
+
+
+          
+            ],
+          ),
+        ),
       ),
     );
   }
