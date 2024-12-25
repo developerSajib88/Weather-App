4285154f8eb02a95557b307b3491554621952ccb
developerSajib88 <developersajib88@gmail.com>
2024-12-25 17:18:32 +0600

    state notifier provider name change.

--- lib/core/dependency_injection/dependency_injection_provider.dart
+++ lib/core/dependency_injection/dependency_injection_provider.dart
@@ -1,6 +1,6 @@
 part of 'dependency_injection.dart';
 
 /// This is my riverpod state provider
-final authenticationProvider =
+final weatherProvider =
 StateNotifierProvider<WeatherStateNotifier,WeatherState>(
         (ref) => WeatherStateNotifier(weatherDomain: getIt())..init());
