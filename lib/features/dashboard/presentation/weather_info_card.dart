a1766aa659ee1ca0f11c1ad7c7984ed8358daed7
developerSajib88 <developersajib88@gmail.com>
2024-12-25 13:09:16 +0600

    Weather info card and weather info widget create.

---
+++ lib/features/dashboard/presentation/weather_info_card.dart
@@ -0,0 +1,45 @@
+import 'package:feature_first/common/widgets/components/background_card.dart';
+import 'package:feature_first/features/dashboard/widget/weather_info.dart';
+import 'package:feature_first/generated/assets.dart';
+import 'package:feature_first/utils/utils.dart';
+import 'package:flutter/material.dart';
+
+class WeatherInfoCard extends StatelessWidget {
+
+  const WeatherInfoCard({super.key,});
+
+  @override
+  Widget build(BuildContext context) {
+    return BackgroundCard(
+      width: 200.w,
+      height: 60.h,
+      child: Row(
+        mainAxisAlignment: mainSpaceAround,
+        children: [
+
+          WeatherInfo(
+            iconImage: Assets.iconsPrecipitation,
+            title: "Precipitation",
+            value: "30%",
+          ),
+
+          WeatherInfo(
+            iconImage: Assets.iconsHumadity,
+            title: "Humidity",
+            value: "30%",
+          ),
+
+
+          WeatherInfo(
+            iconImage: Assets.iconsWindSpeed,
+            title: "Wind Speed",
+            value: "30%",
+          ),
+
+
+
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/features/dashboard/widget/weather_info.dart
@@ -0,0 +1,51 @@
+import 'package:feature_first/app/app.dart';
+import 'package:feature_first/utils/constants/ui_constants.dart';
+import 'package:feature_first/utils/styles/custom_text_styles.dart';
+import 'package:feature_first/utils/utils.dart';
+import 'package:flutter/material.dart';
+
+class WeatherInfo extends StatelessWidget {
+
+  final String iconImage;
+  final String title;
+  final String value;
+
+  const WeatherInfo({
+    super.key,
+    required this.iconImage,
+    required this.title,
+    required this.value
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: crossCenter,
+      mainAxisAlignment: mainCenter,
+      children: [
+
+
+        Image.asset(
+          iconImage,
+          width: 20.w,
+          height: 20.w,
+        ),
+
+        gap4,
+
+        Text(
+          value,
+          style: CustomTextStyles.secondary,
+        ),
+
+        gap4,
+
+        Text(
+          title,
+          style: CustomTextStyles.secondary,
+        )
+        
+      ],
+    );
+  }
+}
