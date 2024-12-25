a673e0913b24308800b2784e472f772b00a9fe3b
developerSajib88 <developersajib88@gmail.com>
2024-12-25 17:19:00 +0600

    Global function create for format date time.

--- lib/common/global/functions/global_functions.dart
+++ lib/common/global/functions/global_functions.dart
@@ -1,1 +1,9 @@
-class GlobalFunctions {}
+import 'package:intl/intl.dart';
+
+class GlobalFunctions {
+
+  static String formatDateTime(String? dateTime){
+    return DateFormat("EEE, dd MMMM yyyy | hh:mm: a").format(DateTime.parse(dateTime?? DateTime.now().toString()));
+  }
+
+}
