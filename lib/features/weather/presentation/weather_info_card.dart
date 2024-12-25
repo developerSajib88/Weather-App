1961852fed66a0e09450e3d94954fc48cd54de07
developerSajib88 <developersajib88@gmail.com>
2024-12-25 17:18:11 +0600

    api urls issue fixed.

--- lib/features/weather/presentation/weather_info_card.dart
+++ lib/features/weather/presentation/weather_info_card.dart
@@ -1,15 +1,19 @@
 import 'package:feature_first/common/widgets/components/background_card.dart';
+import 'package:feature_first/core/dependency_injection/dependency_injection.dart';
 import 'package:feature_first/features/weather/widget/weather_info.dart';
 import 'package:feature_first/generated/assets.dart';
 import 'package:feature_first/utils/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class WeatherInfoCard extends StatelessWidget {
-
-  const WeatherInfoCard({super.key,});
+class WeatherInfoCard extends HookConsumerWidget {
+  const WeatherInfoCard({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final weatherState = ref.watch(weatherProvider);
+
     return BackgroundCard(
       width: 200.w,
       height: 60.h,
@@ -20,20 +24,20 @@ class WeatherInfoCard extends StatelessWidget {
           WeatherInfo(
             iconImage: Assets.iconsPrecipitation,
             title: "Precipitation",
-            value: "30%",
+            value: "${(weatherState.weatherModel?.current?.precipIn ?? 00).toString()}%",
           ),
 
           WeatherInfo(
             iconImage: Assets.iconsHumadity,
             title: "Humidity",
-            value: "30%",
+            value: "${(weatherState.weatherModel?.current?.humidity ?? 00).toString()}%",
           ),
 
 
           WeatherInfo(
             iconImage: Assets.iconsWindSpeed,
             title: "Wind Speed",
-            value: "30%",
+            value: "${(weatherState.weatherModel?.current?.windKph ?? 00).toString()} Km/h",
           ),
 
 
